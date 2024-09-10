4773092bde44580f88f7408affa55a444b643e58
luisroft <luivelasco6541@gmail.com>
2024-09-10 15:07:56 -0500

    Axios library installed - Interface to pokeapi response implemented

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './interfaces/poke-response.interface';
 
 @Injectable()
 export class SeedService {
-  fillDB() {
-    return 'This action fills the DB with data';
+  private readonly axios: AxiosInstance = axios;
+
+  async fillDB() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=1',
+    );
+
+    data.results.forEach(({ name, url }) => {
+      const urlSplitted = url.split('/');
+      const no = +urlSplitted.at(-2);
+
+      console.log(`ID: ${no} - Name ${name}`);
+    });
+
+    return data.results;
   }
 }
